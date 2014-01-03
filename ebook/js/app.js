7132bb91664c07227dbc0b564900b1083a08a51d
tonytony29 <tonytony29@users.noreply.github.com>
2014-01-03 11:27:06 +0800

    Update app.js
    code review

--- ebook/js/app.js
+++ ebook/js/app.js
@@ -5,26 +5,42 @@ function listContents(storagename) {
         var files = navigator.getDeviceStorage(storagename);
  
         var cursor = files.enumerate();
- 
+		
+		//alert (files[0]);
+		var arr=[];
         cursor.onsuccess = function () {
                 //alert("Got something");
                 var file = this.result;
-                   
+ 
                 if (file != null) {
-                    var imageElement = $('<img height="50" width="50">');
-                    imageElement.attr('src', window.URL.createObjectURL(file));
+                    //var imageElement = $('<img height="50" width="50">');
+                    //imageElement.attr('src', window.URL.createObjectURL(file));
                     $("<p>" + file.name + file.type + "</p>").appendTo('#results');
-                    imageElement.appendTo("#results");
+                    //imageElement.appendTo("#results");
+					//alert (file.name);
+					//var path = "C:////+" + file.name;
+					
+					arr.push (file.name);
+					
                     this.done = false;
-                    }
+                }
         else {
-                this.done = true;
-             }
+			this.done = true;
+        }
  
         if (!this.done) {
                 this.continue();
-             }
+        }
+		
+		$('#id2').text (arr[0]);
+		$('#id3').text (arr[1]);
+		$('#id4').text (arr[2]);
+		$('#id5').text (arr[3]);
+		
+		//$('#id3').text (arr[1]);
    }
+   
+   
 }
 $(document).ready(function(){
  
