40a168ab723c803f1ff53dbb8bd204be07d426e2
tonytony29 <tonytony29@hotmail.com>
2014-01-06 00:39:11 +0800

    Setting04

--- ebook/js/setting.js
+++ ebook/js/setting.js
@@ -20,26 +20,36 @@ $("#11").click(function() {
 	 $("#testword").css("color","black");
 	 })
 $("#12").click(function() {
-	 $("#testword").css("color","green");
+	 $("#testword").css("color","#39DF3B");//green
 	 })
 $("#13").click(function() {
-	 $("#testword").css("color","blue");
+	 $("#testword").css("color","#1989FF");//blue
 	 })
 $("#14").click(function() {
 	 $("#testword").css("color","white");
 	 })
 $("#21").click(function() {
-	 $("#testword").css("background-color","black");
+	 $("#testword").css("background","#332B33");//black
 	 })
 $("#22").click(function() {
-	 $("#testword").css("background-color","green");
+	 $("#testword").css("background","#00A002");//green
 	 })	
 $("#23").click(function() {
-	 $("#testword").css("background-color","blue");
+	 $("#testword").css("background","#004EA0");//blue
 	 })
 $("#24").click(function() {
-	 $("#testword").css("background-color","white");
+	 $("#testword").css("background","#F1F1F1");//white
 	 })	 
+$("#makesure").click(function() {
+     var cssFontSize = $("#testword").css("font-size");
+     var fontSize = parseFloat(cssFontSize);
+	 var unit = cssFontSize.slice(-2);
+	 $(".tab").css("font-size", fontSize + unit);
+	 var color = $("#testword").css("color");
+	 $(".tab").css("color",color);
+	 var bcolor = $("#testword").css("background-color");
+     $(".tab").css("background",bcolor);
+	 })
 	 
 $(read_window).scroll(function () {
      var scrollVal = $(this).scrollTop();
